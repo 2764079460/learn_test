532af886506ce1e55823f82319a4f106bd9dcae4
2764079460 <2764079460@qq.com>
2020-06-24 14:08:20 +0800

    新建

---
+++ eureka-server/src/main/java/com/vueblog/config/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.vueblog.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by xiangmiao on 2020/6/23.
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
